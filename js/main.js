4faeb69b8465842291784b410bd4aa57e37692c7
Alan Baumert <alan.baumert@hotmail.com>
2023-03-31 10:29:28 -0300

    commit-1

---
+++ js/main.js
@@ -0,0 +1,12 @@
+$('.slider-principal').slick({
+    dots: true,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 1,
+    slideToScroll: 1,
+    adaptiveHeight: true,
+    autoplay: true,
+    autoplayspeed: 2000
+  });
+
+                  
